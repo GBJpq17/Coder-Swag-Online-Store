5d6b10ce8938d8187d6da087b100365d5a05800e
Ryan Loera <rloera@csumb.edu>
2017-08-10 21:45:23 -0400

    initial product ready

---
+++ Coder Swag/Controller/CategoriesVC.swift
@@ -0,0 +1,52 @@
+//
+//  CategoriesVC.swift
+//  Coder Swag
+//
+//  Created by RyLo on 8/8/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            let barButton = UIBarButtonItem()
+            barButton.title = ""
+            navigationItem.backBarButtonItem = barButton
+            
+            assert(sender as? Category != nil)
+            productsVC.initializeProducts(category: sender as! Category)
+        }
+    }
+
+}
+
---
+++ Coder Swag/Controller/ProductsVC.swift
@@ -0,0 +1,41 @@
+//
+//  ProductsVC.swift
+//  Coder Swag
+//
+//  Created by RyLo on 8/10/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    private(set) public var products = [Product]()
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+    }
+    
+    func initializeProducts(category: Category) {
+        products = DataService.instance.getProduct(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        return ProductCell()
+    }
+
+}
